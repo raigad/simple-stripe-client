4ba6ae1164961c010db6311ce0c869a980467b7e
Rohit <rohitdeshmukh69@gmail.com>
2019-02-01 20:04:08 +0000

    updated

--- example.py
+++ example.py
@@ -8,40 +8,42 @@ STRIPE_SECRET_KEY = config('STRIPE_SECRET_KEY')
 
 stripe_api = simple_stripe_client.Api(STRIPE_SECRET_KEY, debug_http=True)
 
-CUSTOMER_DATA ={
+CUSTOMER_DATA = {
 
 }
 
-#create customer
-#customer = stripe_api.customers.post()
+# create customer
+# customer = stripe_api.customers.post()
 
-#update customer
+# update customer
 customer_id = 'cus_AUVASurSCMYLb7'
 
-#get bank accounts
-#accounts = stripe_api.customers.id(customer_id).sources.get(object='bank_account',limit=3)
-#print(accounts)
+# get bank accounts
+# accounts = stripe_api.customers.id(customer_id).sources.get(object='bank_account',limit=3)
+# accounts = stripe_api.customers.id(customer_id).sources
+# print(repr(stripe_api))
+# print(accounts)
 
 # customer = stripe_api.customers.id(customer_id).post(description="This is the test customer created",metadata={ 'name' : 'RohitX', 'customer_id' : 123,'last_name':'desf'},email='rohit@example.com')
 # print(customer)
 #
-#DELETE Customer
+# DELETE Customer
 # customer_del = stripe_api.customers.id(customer_id).delete()
 # print("\n\n\n\n")
 # print(customer_del)
 
 
-#print("\n\n\n")
+# print("\n\n\n")
 # print(stripe_api.charges.get)
 # data = stripe_api.charges.get(limit=1)
-#charge_id = 'ch_1Dz60xJKzeKazErxinipPnZa'
-#data = stripe_api.charges.id(charge_id).post(description='this is test description x')
-#print(data)
-#data = stripe_api.charges.id(charge_id).get()
-#print("\n\n\n")
-#print(data['description'])
-#print("\n\n\n")
-#print(data['metadata'])
+# charge_id = 'ch_1Dz60xJKzeKazErxinipPnZa'
+# data = stripe_api.charges.id(charge_id).post(description='this is test description x')
+# print(data)
+# data = stripe_api.charges.id(charge_id).get()
+# print("\n\n\n")
+# print(data['description'])
+# print("\n\n\n")
+# print(data['metadata'])
 
 ##Create new charge
 # CHARGE_DATA = {
